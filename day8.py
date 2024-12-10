a3bbc5ebb3aee04c998a47e5bed60243595cea7e
Benjamin Michael <ben29michael@gmail.com>
2024-12-09 21:07:44 -0800

    day 8 part one, now need to study for finals 😩

--- day8.py
+++ day8.py
@@ -2,44 +2,38 @@ import os
 from collections import defaultdict
 
 with open(os.path.join(os.getcwd(), 'resources', 'day8.txt')) as file:
-    lines = [line for line in file if not line.isspace()]
+    lines = [line.strip() for line in file if not line.isspace()]
 
-antennas = defaultdict(set)
+antennas = defaultdict(list)
 for i, row in enumerate(lines):
     for j, char in enumerate(row):
         if char != '.':
-            antennas[char].add((i, j))
+            antennas[char].append((i, j))
 
-# Get the distance between two coordinates in a cartesian coordinate system
-def cartesian_distance(p1: tuple[int, int], p2: tuple[int, int]):
+dim = (len(lines), len(lines[0]))
+
+def add_tuples(tup1, tup2):
+    return tuple(map(sum, zip(tup1, tup2)))
+
+def cartesian_distance(p1, p2):
     return p1[0] - p2[0], p1[1] - p2[1]
 
-def puzzle_one() -> int:
-    """
-    test input, should find 14 anti-nodes:
-    ............
-    ........0...
-    .....0......
-    .......0....
-    ....0.......
-    ......A.....
-    ............
-    ............
-    ........A...
-    .........A..
-    ............
-    ............
+def in_bounds(point):
+    return 0 <= point[0] < dim[0] and 0 <= point[1] < dim[1]
 
-    antennas = map of all unique locations each antenna type occurs at
-    locations = set of unique locations that an anti-node exists at
-    for each antenna in antennas
-        for each location in antenna
-            create two anti-nodes, where the anti-node is placed along the line connecting the antenna pair and is at
-            least as far away as the distance between the pair (excluding locations off the map, including locations
-            that might cover an existing antenna)
+def generate_antinodes(p1: tuple[int, int], p2: tuple[int, int]):
+     antinodes = [add_tuples(p1, cartesian_distance(p1, p2)), add_tuples(p2, cartesian_distance(p2, p1))]
+     return [p for p in antinodes if in_bounds(p)]
+
+def puzzle_one() -> int:
+    locations = set()
+    for antenna, nodes in antennas.items():
+        for index in range(len(nodes)):
+            for node in nodes[index + 1:]:
+                antinodes = generate_antinodes(nodes[index], node)
+                locations.update(antinodes)
+
     return len(locations)
-    """
-    return 0
 
 def puzzle_two() -> int:
     return 0
